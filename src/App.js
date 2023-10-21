35ce134aa2b72c44a4a7ae297a87bfd57a29ceb1
Elisabeth <corderoelisabeth83@gmail.com>
2023-10-21 16:09:34 -0300

    Implemento Contex

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import { CartProvider } from './context/CartContext';
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <NavBar/>
-        <Routes>
-          <Route path="/" element={<ItemListContainer greeting = {''}/>} />
-          <Route path="categorias/:categoryId" element={<ItemListContainer/>} />
-          <Route path="item/:itemId" element={<ItemDetailContainer/>} />
-          <Route path="*" element={<h1>Not Found!</h1>} />
-        </Routes>
+        <CartProvider>
+          <NavBar/>
+          <Routes>
+            <Route path="/" element={<ItemListContainer greeting = {''}/>} />
+            <Route path="categorias/:categoryId" element={<ItemListContainer/>} />
+            <Route path="item/:itemId" element={<ItemDetailContainer/>} />
+            <Route path="*" element={<h1>Not Found!</h1>} />
+          </Routes>
+        </CartProvider>
       </BrowserRouter>
     </div>
   );
